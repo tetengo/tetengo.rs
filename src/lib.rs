b818f307d1c9414e7dcfa12a8ab1e81e83541d50
kaoru <kaorut@mc.pikara.ne.jp>
2023-03-21 03:25:27 +0900

    Implement Storage #1

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 //! # Tetengo Trie
-//! 
+//!
 //! A trie library implement with double arrays.
-//! 
+//!
 //! Copyright 2023 kaoru <https://www.tetengo.org/>
 
 pub mod storage;
-
 
 /*
 pub fn add(left: usize, right: usize) -> usize {
