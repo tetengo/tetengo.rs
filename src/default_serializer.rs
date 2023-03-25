1b82604b481365c4a7d59e2da3fcb965cfa1d932
kaoru <kaorut@mc.pikara.ne.jp>
2023-03-25 19:12:23 +0900

    Create DefaultSerializer #11

---
+++ src/default_serializer.rs
@@ -0,0 +1,15 @@
+/*!
+    # Default Serializer
+
+    Copyright 2023 kaoru  <https://www.tetengo.org/>
+*/
+
+/**
+    # Default Serializer
+*/
+pub struct DefaultSerializer {}
+
+impl DefaultSerializer {}
+
+#[cfg(test)]
+mod tests {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![doc = include_str!("../README.md")]
 
+pub mod default_serializer;
 pub mod storage;
 
 /*
