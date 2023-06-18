2e574d357ce50597117dd0348bd2ac854a4c1477
kaoru <kaorut@mc.pikara.ne.jp>
2023-06-18 13:14:26 +0900

    Implement MmapStorage #19

--- tetengo_trie/src/mmap_storage.rs
+++ tetengo_trie/src/mmap_storage.rs
@@ -4,6 +4,8 @@
  * Copyright 2023 kaoru  <https://www.tetengo.org/>
  */
 
+use tempfile as _;
+
 /**
  * An mmap storage.
  *
@@ -31,10 +33,38 @@ impl<T: Default> MmapStorage<T> {
 
 #[cfg(test)]
 mod tests {
+    use std::fs::File;
+
+    use tempfile::tempfile;
+
     use super::*;
+
+    fn make_temporary_path(_initial_content: &[u8]) -> File {
+        let file = tempfile().expect("Can't create a temporary file.");
+        file
+    }
 
     #[test]
     fn new() {
+        let _file = make_temporary_path(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
         let _ = MmapStorage::<u32>::new();
+
+        // {
+        //     const auto file_path = temporary_file_path(serialized_fixed_value_size);
+        //     BOOST_SCOPE_EXIT(&file_path)
+        //     {
+        //         std::filesystem::remove(file_path);
+        //     }
+        //     BOOST_SCOPE_EXIT_END;
+
+        //     const boost::interprocess::file_mapping file_mapping{ file_path.c_str(), boost::interprocess::read_only };
+        //     const auto                        file_size = static_cast<std::size_t>(std::filesystem::file_size(file_path));
+        //     tetengo::trie::value_deserializer deserializer{ [](const std::vector<char>& serialized) {
+        //         static const tetengo::trie::default_deserializer<std::uint32_t>uint32_deserializer{ false };
+        //         return uint32_deserializer(serialized);
+        //     } };
+        //     const tetengo::trie::mmap_storage storage{ file_mapping, 0, file_size, std::move(deserializer) };
+        // }
     }
 }
