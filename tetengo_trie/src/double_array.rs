8221aa8fdbd7ff76af920f40d20912e63d8bf4d9
kaoru <kaorut@mc.pikara.ne.jp>
2023-06-30 02:33:23 +0900

    Implement DoubleArray #23

--- tetengo_trie/src/double_array.rs
+++ tetengo_trie/src/double_array.rs
@@ -8,15 +8,55 @@ use std::fmt::{self, Debug, Formatter};
 
 use crate::storage::Storage;
 
+/// The double array element type.
+pub type DoubleArrayElement<'a> = (&'a str, i32);
+/**
+ * A building observer set.
+ */
+pub struct BuldingObserverSet {
+    _adding: Box<dyn FnOnce(&DoubleArrayElement<'_>)>,
+    _done: Box<dyn FnOnce()>,
+}
+
+impl BuldingObserverSet {
+    /**
+     * Creates a building observer set.
+     *
+     * # Parameters
+     * * `adding` - An adding observer.
+     * * `done` - A done observer.
+     */
+    pub fn new(adding: Box<dyn FnOnce(&DoubleArrayElement<'_>)>, done: Box<dyn FnOnce()>) -> Self {
+        Self {
+            _adding: adding,
+            _done: done,
+        }
+    }
+}
+
+impl Debug for BuldingObserverSet {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("BuldingObserverSet")
+            .field("adding", &"Box<dyn FnOnce(&DoubleArrayElement<'_>)>")
+            .finish()
+    }
+}
+
 /**
  * A double array.
- *
- * # Type Parameters
- * * `V` - A value type.
  */
 pub struct DoubleArray<V> {
     _storage: Box<dyn Storage<V>>,
     _root_base_check_index: usize,
+}
+
+impl<V> DoubleArray<V> {
+    /**
+     * Creates a double array.
+     */
+    fn _new() -> Self {
+        todo!()
+    }
 }
 
 impl<V> Debug for DoubleArray<V> {
@@ -29,8 +69,12 @@ impl<V> Debug for DoubleArray<V> {
 
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn new() {
         // TODO: Implement it.
+        // let double_array = DoubleArray::<i32>::new();
+
+        // assert_eq!(base_check_array_of(double_array.storage()), EXPECTED_EMPTY_BASE_CHECK_ARRAY_EMPTY);
     }
 }
