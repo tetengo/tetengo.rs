239bb6f2cb3d4314405aa1e85f798817246c897d
kaoru <kaorut@mc.pikara.ne.jp>
2023-07-03 03:38:34 +0900

    Implement DoubleArray #23

--- tetengo_trie/src/memory_storage.rs
+++ tetengo_trie/src/memory_storage.rs
@@ -677,23 +677,23 @@ mod tests {
         }
     }
 
-    // #[test]
-    // fn clone() {
-    //     let mut storage = MemoryStorage::<u32>::new();
+    #[test]
+    fn clone_box() {
+        let mut storage = MemoryStorage::<u32>::new();
 
-    //     storage.set_base_at(0, 42).unwrap();
-    //     storage.set_base_at(1, 0xFE).unwrap();
-    //     storage.set_check_at(1, 24).unwrap();
+        storage.set_base_at(0, 42).unwrap();
+        storage.set_base_at(1, 0xFE).unwrap();
+        storage.set_check_at(1, 24).unwrap();
 
-    //     let clone = storage.clone();
+        let clone = storage.clone_box();
 
-    //     let base_check_array = base_check_array_of(&clone);
+        let base_check_array = base_check_array_of(&*clone);
 
-    //     #[rustfmt::skip]
-    //     const EXPECTED: [u32; 2] = [
-    //         0x00002AFFu32,
-    //         0x0000FE18u32,
-    //     ];
-    //     assert_eq!(base_check_array, &EXPECTED);
-    // }
+        #[rustfmt::skip]
+        const EXPECTED: [u32; 2] = [
+            0x00002AFFu32,
+            0x0000FE18u32,
+        ];
+        assert_eq!(base_check_array, &EXPECTED);
+    }
 }
