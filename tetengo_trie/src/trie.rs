00314009a9ce75f787dddc3dcbda8b81c5322d16
kaoru <kaorut@mc.pikara.ne.jp>
2023-07-09 21:33:20 +0900

    Implement Trie #23

--- tetengo_trie/src/trie.rs
+++ tetengo_trie/src/trie.rs
@@ -4,7 +4,16 @@
  * Copyright 2023 kaoru  <https://www.tetengo.org/>
  */
 
-use crate::double_array::DEFAULT_DENSITY_FACTOR;
+use crate::double_array::{DoubleArray, DEFAULT_DENSITY_FACTOR};
+use crate::serializer::{Serializer, SerializerOf};
+
+/**
+ * A result type.
+ *
+ * # Type Parameters
+ * * `T` - A type.
+ */
+pub type Result<T> = anyhow::Result<T>;
 
 /// The default double array density factor.
 const _DEFAULT_DOUBLE_ARRAY_DENSITY_FACTOR: usize = DEFAULT_DENSITY_FACTOR;
@@ -12,15 +21,25 @@ const _DEFAULT_DOUBLE_ARRAY_DENSITY_FACTOR: usize = DEFAULT_DENSITY_FACTOR;
 /**
  * A trie.
  */
-#[derive(Debug, Clone, Copy, Default)]
-pub struct Trie {}
+#[derive(Debug)]
+pub struct Trie<Key: ?Sized, Value, KeySerializer: Serializer = <() as SerializerOf<Key>>::Type> {
+    _phantom: std::marker::PhantomData<Key>,
+    _double_array: DoubleArray<Value>,
+    _key_serializer: KeySerializer,
+}
 
-impl Trie {
+impl<Key: ?Sized, Value: Clone + 'static, KeySerializer: Serializer>
+    Trie<Key, Value, KeySerializer>
+{
     /**
      * Creates a trie.
      */
-    pub fn new() -> Self {
-        Self {}
+    pub fn new() -> Result<Self> {
+        Ok(Self {
+            _phantom: std::marker::PhantomData,
+            _double_array: DoubleArray::new()?,
+            _key_serializer: KeySerializer::new(true),
+        })
     }
 }
 
@@ -30,6 +49,6 @@ mod tests {
 
     #[test]
     fn test_new() {
-        let _ = Trie::new();
+        let _trie = Trie::<str, i32>::new().unwrap();
     }
 }
