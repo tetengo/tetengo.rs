5e96169ac5dbc34b0d6487a09f3ec8d9a5b26d49
kaoru <kaorut@mc.pikara.ne.jp>
2023-07-10 00:17:04 +0900

    Implement Trie #28

--- tetengo_trie/src/trie.rs
+++ tetengo_trie/src/trie.rs
@@ -22,24 +22,18 @@ const _DEFAULT_DOUBLE_ARRAY_DENSITY_FACTOR: usize = DEFAULT_DENSITY_FACTOR;
  * A trie.
  */
 #[derive(Debug)]
-pub struct Trie<Key: ?Sized, Value, KeySerializer: Serializer = <() as SerializerOf<Key>>::Type> {
+pub struct Trie<Key, Value, KeySerializer: Serializer = <() as SerializerOf<Key>>::Type> {
     _phantom: std::marker::PhantomData<Key>,
     _double_array: DoubleArray<Value>,
     _key_serializer: KeySerializer,
 }
 
-impl<Key: ?Sized, Value: Clone + 'static, KeySerializer: Serializer>
-    Trie<Key, Value, KeySerializer>
-{
+impl<Key, Value: Clone + 'static, KeySerializer: Serializer> Trie<Key, Value, KeySerializer> {
     /**
      * Creates a trie.
      */
     pub fn new() -> Result<Self> {
-        Ok(Self {
-            _phantom: std::marker::PhantomData,
-            _double_array: DoubleArray::new()?,
-            _key_serializer: KeySerializer::new(true),
-        })
+        Self::new_with_keyserializer(KeySerializer::new(true))
     }
 
     /**
@@ -52,6 +46,47 @@ impl<Key: ?Sized, Value: Clone + 'static, KeySerializer: Serializer>
         Ok(Self {
             _phantom: std::marker::PhantomData,
             _double_array: DoubleArray::new()?,
+            _key_serializer: key_serializer,
+        })
+    }
+
+    /**
+     * Creates a trie.
+     *
+     * # Arguments
+     * * `elements` - Elements.
+     */
+    pub fn new_with_elements(elements: Vec<(KeySerializer::Object<'_>, Value)>) -> Result<Self> {
+        let key_serializer = KeySerializer::new(true);
+
+        let mut double_array_content_keys = Vec::<String>::with_capacity(elements.len());
+        for element in &elements {
+            let (key, _) = &element;
+            let serialized_key = String::from_utf8(key_serializer.serialize(key))?;
+            double_array_content_keys.push(serialized_key);
+        }
+        let mut double_array_contents = Vec::<(&str, i32)>::with_capacity(elements.len());
+        for (i, _) in elements.iter().enumerate() {
+            double_array_contents.push((&double_array_content_keys[i], i as i32));
+        }
+
+        //     const double_array::building_observer_set_type double_array_building_observer_set{
+        //         [&building_observer_set](const std::pair<std::string_view, std::int32_t>& element) {
+        //             building_observer_set.adding(element.first);
+        //         },
+        //         [&building_observer_set]() { building_observer_set.done(); }
+        //     };
+
+        let mut double_array = DoubleArray::<Value>::new_with_elements(double_array_contents)?;
+
+        for (i, element) in elements.into_iter().enumerate() {
+            let (_, value) = element;
+            double_array.storage_mut().add_value_at(i, value)?;
+        }
+
+        Ok(Self {
+            _phantom: std::marker::PhantomData,
+            _double_array: double_array,
             _key_serializer: key_serializer,
         })
     }
@@ -72,5 +107,12 @@ mod tests {
     fn new_with_keyserializer() {
         let key_serializer = StringSerializer::new(true);
         let _trie = Trie::<&str, i32>::new_with_keyserializer(key_serializer).unwrap();
+    }
+
+    #[test]
+    fn new_with_elements() {
+        let _trie =
+            Trie::<&str, i32>::new_with_elements([("Kumamoto", 42), ("Tamana", 24)].to_vec())
+                .unwrap();
     }
 }
