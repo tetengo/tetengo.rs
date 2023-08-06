7a44b4b6829befde85b714da383a5fb0197f0f75
kaoru <kaorut@mc.pikara.ne.jp>
2023-08-06 11:41:42 +0900

    Create a test case #32

---
+++ tetengo_trie/tests/usage.rs
@@ -0,0 +1,12 @@
+use anyhow as _;
+use hashlink as _;
+use memmap2 as _;
+use once_cell as _;
+use tempfile as _;
+use tetengo_trie as _;
+use thiserror as _;
+
+mod usage {
+    #[test]
+    fn usage() {}
+}
