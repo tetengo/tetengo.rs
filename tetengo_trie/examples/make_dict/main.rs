de5d9422780d987191188dfd880203c36ca0ad7d
kaoru <kaorut@mc.pikara.ne.jp>
2023-09-24 15:26:51 +0900

    Implement make_dict #34

--- tetengo_trie/examples/make_dict/main.rs
+++ tetengo_trie/examples/make_dict/main.rs
@@ -4,6 +4,16 @@
  * Copyright 2023 kaoru  <https://www.tetengo.org/>
  */
 
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<_>>();
+    if args.len() <= 2 {
+        eprintln!("Usage: make_dict UniDic_lex.csv trie.bin");
+        // return;
+    }
+
+    // const auto word_offset_map = load_lex_csv(argv[1]);
+    // const auto p_trie = build_trie(word_offset_map);
+    // serialize_trie(*p_trie, argv[2]);
 }
