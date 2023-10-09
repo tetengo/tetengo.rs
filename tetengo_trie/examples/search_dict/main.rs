7dc33ad5ca794235b3ba05ac0a08cf1b42ed1f11
kaoru <kaorut@mc.pikara.ne.jp>
2023-10-09 17:24:17 +0900

    Implement search_dict #35

--- tetengo_trie/examples/search_dict/main.rs
+++ tetengo_trie/examples/search_dict/main.rs
@@ -4,13 +4,16 @@
  * Copyright 2023 kaoru  <https://www.tetengo.org/>
  */
 
+use std::cmp::min;
 use std::env;
 use std::fs::File;
 use std::io::{stdin, Read};
+use std::mem::size_of;
 use std::path::Path;
 use std::process::exit;
 
 use anyhow::Result;
+use tetengo_trie::{MemoryStorage, Trie, ValueDeserializer};
 
 fn main() {
     if let Err(e) = main_core() {
@@ -27,6 +30,7 @@ fn main_core() -> Result<()> {
     }
 
     let _lex_csv = load_lex_csv(Path::new(&args[1]))?;
+    let _trie = load_trie(Path::new(&args[2]))?;
 
     loop {
         eprint!(">> ");
@@ -103,4 +107,50 @@ fn load_lex_csv(lex_csv_path: &Path) -> Result<String> {
         return Err(DictSearchingError::CantReadWholeOfLexCsvFile.into());
     }
     Ok(buffer)
+}
+
+type DictTrie = Trie<String, Vec<(usize, usize)>>;
+
+fn load_trie(trie_path: &Path) -> Result<DictTrie> {
+    let mut file = File::open(trie_path)?;
+
+    let value_deserializer = ValueDeserializer::new(deserialize_value);
+    let storage = Box::new(MemoryStorage::from_reader(&mut file, &value_deserializer)?);
+    let trie = DictTrie::builder_with_storage(storage).build();
+    Ok(trie)
+}
+
+const VALUE_CAPACITY: usize = 4usize;
+
+fn deserialize_value(bytes: &[u8]) -> Result<Vec<(usize, usize)>> {
+    let mut byte_offset = 0usize;
+
+    let mut vps = Vec::new();
+
+    let size = deserialize_usize(bytes, &mut byte_offset)?;
+    vps.reserve(size);
+    for _ in 0..min(size, VALUE_CAPACITY) {
+        vps.push(deserialize_pair_of_usize(bytes, &mut byte_offset)?);
+    }
+    (VALUE_CAPACITY..size).for_each(|_| {
+        vps.push((0, 0));
+    });
+
+    Ok(vps)
+}
+
+fn deserialize_pair_of_usize(bytes: &[u8], byte_offset: &mut usize) -> Result<(usize, usize)> {
+    let first = deserialize_usize(bytes, byte_offset)?;
+    let second = deserialize_usize(bytes, byte_offset)?;
+    Ok((first, second))
+}
+
+fn deserialize_usize(bytes: &[u8], byte_offset: &mut usize) -> Result<usize> {
+    let mut value = 0usize;
+    (0..size_of::<u32>()).for_each(|i| {
+        value <<= 8;
+        value |= bytes[*byte_offset + i] as usize;
+    });
+    *byte_offset += size_of::<u32>();
+    Ok(value)
 }
