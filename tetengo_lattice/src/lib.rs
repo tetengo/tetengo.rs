16c99d350dfa72d98dcf9fe7a764b335dda2eb68
kaoru <kaorut@mc.pikara.ne.jp>
2023-11-01 16:22:20 +0900

    Craete a crate tetengo_lattice #45

---
+++ tetengo_lattice/src/lib.rs
@@ -0,0 +1,21 @@
+/*!
+ * A lattice library.
+ */
+
+/**
+ * Adds two numbers.
+ */
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
