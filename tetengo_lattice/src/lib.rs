59958d99ce26b90ed84c7af923dd2ae6f15f893e
kaoru <kaorut@mc.pikara.ne.jp>
2023-11-12 16:50:56 +0900

    Implement Node #52

--- tetengo_lattice/src/lib.rs
+++ tetengo_lattice/src/lib.rs
@@ -6,9 +6,11 @@
 
 pub mod entry;
 pub mod input;
+pub mod node;
 pub mod string_input;
 
 pub use entry::Entry;
 pub use input::Input;
 pub use input::InputError;
+pub use node::Node;
 pub use string_input::StringInput;
---
+++ tetengo_lattice/src/node.rs
@@ -0,0 +1,11 @@
+/*!
+ * A node.
+ *
+ * Copyright 2023 kaoru  <https://www.tetengo.org/>
+ */
+
+/**
+ * A node.
+ */
+#[derive(Debug, Clone, Copy)]
+pub struct Node {}
