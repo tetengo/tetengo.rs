d0de89b52c25e425e191321a23811450ee632ec3
kaoru <kaorut@mc.pikara.ne.jp>
2023-12-03 11:20:44 +0900

    Implement Vocabulary #56

--- tetengo_lattice/src/vocabulary.rs
+++ tetengo_lattice/src/vocabulary.rs
@@ -4,35 +4,35 @@
  * Copyright 2023 kaoru  <https://www.tetengo.org/>
  */
 
+use crate::connection::Connection;
+use crate::entry::EntryView;
+use crate::input::Input;
+use crate::node::Node;
+
 /**
  * A vocabulary.
  */
 pub trait Vocabulary {
-    /*
-        /*!
-            \brief Creates a vocabulary.
-        */
-        vocabulary();
-    */
-    /*
-        /*!
-            \brief Finds entries.
+    /**
+     * Finds entries.
+     *
+     * # Arguments
+     * * `key` - A key.
+     *
+     * # Returns
+     * Entry views.
+     */
+    fn find_entries(&self, key: &dyn Input) -> Vec<EntryView<'_>>;
 
-            \param key A key.
-
-            \return Entry views.
-        */
-        [[nodiscard]] std::vector<entry_view> find_entries(const input& key) const;
-    */
-    /*
-        /*!
-            \brief Finds a connection between an origin node and a destination entry.
-
-            \param from An origin node.
-            \param to   A destination entry.
-
-            \return A connection between the origin node and the destination entry.
-        */
-        [[nodiscard]] connection find_connection(const node& from, const entry_view& to) const;
-    */
+    /**
+     * Finds a connection between an origin node and a destination entry.
+     *
+     * # Arguments
+     * * `from` - An origin node.
+     * * `to`   - A destination entry.
+     *
+     * # Returns
+     * A connection between the origin node and the destination entry.
+     */
+    fn find_connection(&self, from: &Node<'_>, to: &EntryView<'_>) -> Connection;
 }
