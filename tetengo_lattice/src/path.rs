1ef4afbecfdf6fa3f9d46c132b58bcb437188928
kaoru <kaorut@mc.pikara.ne.jp>
2024-06-17 01:35:14 +0900

    Implement Path #66

--- tetengo_lattice/src/path.rs
+++ tetengo_lattice/src/path.rs
@@ -4,99 +4,91 @@
  * Copyright (C) 2023-2024 kaoru  <https://www.tetengo.org/>
  */
 
+use crate::node::Node;
+
 /**
  * A path.
  */
-#[derive(Debug, Copy, Clone)]
-pub struct Path {}
+#[derive(Debug, Default)]
+pub struct Path<'a> {
+    _nodes: Vec<Node<'a>>,
+    _cost: i32,
+}
 
-/*
-   private:
-       // variables
+impl Path<'_> {
+    /**
+     * Creates an empty path.
+     */
+    pub fn new() -> Self {
+        Path {
+            _nodes: Vec::new(),
+            _cost: 0,
+        }
+    }
 
-       std::vector<node> m_nodes;
+    /*
+           /*!
+               \brief Creates a path.
 
-       int m_cost;
-   };
-*/
+               \param nodes Nodes.
+               \param cost  A cost.
+           */
+           path(std::vector<node> nodes, int cost);
+    */
+    /*
+       path::path(std::vector<node> nodes, const int cost) : m_nodes{ std::move(nodes) }, m_cost{ cost } {}
+    */
+    /*
+           // functions
 
-/*
-   /*!
-       \brief A path.
-   */
-   class path
-   {
-   public:
-       // constructors and destructor
+           /*!
+               \brief Returns true when this path is empty.
 
-       /*!
-           \brief Creates an empty path.
-       */
-       path();
-*/
-/*
-       /*!
-           \brief Creates a path.
+               \retval true  When this path is empty.
+               \retval false Otherwise.
+           */
+           [[nodiscard]] bool empty() const;
+    */
+    /*
+       bool path::empty() const
+       {
+           return std::empty(m_nodes);
+       }
+    */
+    /*
+           /*!
+               \brief Returns the nodes.
 
-           \param nodes Nodes.
-           \param cost  A cost.
-       */
-       path(std::vector<node> nodes, int cost);
-*/
-/*
-       // functions
+               \return The nodes.
+           */
+           [[nodiscard]] const std::vector<node>& nodes() const;
+    */
+    /*
+       const std::vector<node>& path::nodes() const
+       {
+           return m_nodes;
+       }
+    */
+    /*
+           /*!
+               \brief Returns the cost.
 
-       /*!
-           \brief Returns true when this path is empty.
-
-           \retval true  When this path is empty.
-           \retval false Otherwise.
-       */
-       [[nodiscard]] bool empty() const;
-*/
-/*
-       /*!
-           \brief Returns the nodes.
-
-           \return The nodes.
-       */
-       [[nodiscard]] const std::vector<node>& nodes() const;
-*/
-/*
-       /*!
-           \brief Returns the cost.
-
-           \return The cost.
-       */
-       [[nodiscard]] int cost() const;
-*/
-/*
-   path::path() : m_nodes{}, m_cost{ 0 } {}
-*/
-/*
-   path::path(std::vector<node> nodes, const int cost) : m_nodes{ std::move(nodes) }, m_cost{ cost } {}
-*/
-/*
-   bool path::empty() const
-   {
-       return std::empty(m_nodes);
-   }
-*/
-/*
-   const std::vector<node>& path::nodes() const
-   {
-       return m_nodes;
-   }
-*/
-/*
-   int path::cost() const
-   {
-       return m_cost;
-   }
-*/
+               \return The cost.
+           */
+           [[nodiscard]] int cost() const;
+    */
+    /*
+       int path::cost() const
+       {
+           return m_cost;
+       }
+    */
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     /*
     namespace
     {
@@ -137,36 +129,6 @@ mod tests {
         }
     */
     /*
-        tetengo_lattice_node_t to_c_node(const tetengo::lattice::node& cpp_node)
-        {
-            tetengo_lattice_node_t c_node{};
-
-            c_node.key_handle = reinterpret_cast<tetengo_lattice_entryView_keyHandle_t>(cpp_node.p_key());
-            c_node.value_handle = reinterpret_cast<tetengo_lattice_entryView_valueHandle_t>(&cpp_node.value());
-            c_node.index_in_step = cpp_node.index_in_step();
-            c_node.preceding_step = cpp_node.preceding_step();
-            c_node.p_preceding_edge_costs = std::data(cpp_node.preceding_edge_costs());
-            c_node.preceding_edge_cost_count = std::size(cpp_node.preceding_edge_costs());
-            c_node.best_preceding_node = cpp_node.best_preceding_node();
-            c_node.node_cost = cpp_node.node_cost();
-            c_node.path_cost = cpp_node.path_cost();
-
-            return c_node;
-        }
-    */
-    /*
-        const std::vector<tetengo_lattice_node_t>& c_nodes()
-        {
-            static const std::vector<tetengo_lattice_node_t> singleton{ []() {
-                std::vector<tetengo_lattice_node_t>          nodes{};
-                nodes.reserve(std::size(cpp_nodes()));
-                std::transform(std::begin(cpp_nodes()), std::end(cpp_nodes()), std::back_inserter(nodes), to_c_node);
-                return nodes;
-            }() };
-            return singleton;
-        }
-    */
-    /*
         bool equal_nodes(const std::vector<tetengo_lattice_node_t>& one, const std::vector<tetengo_lattice_node_t>& another)
         {
             if (std::size(one) != std::size(another))
@@ -188,6 +150,11 @@ mod tests {
 
     }
     */
+
+    #[test]
+    fn new() {
+        let _path = Path::new();
+    }
     /*
     BOOST_AUTO_TEST_SUITE(test_tetengo)
     BOOST_AUTO_TEST_SUITE(lattice)
@@ -203,32 +170,6 @@ mod tests {
         }
         {
             const tetengo::lattice::path path_{ cpp_nodes(), 42 };
-        }
-
-        {
-            const auto* const p_path = tetengo_lattice_path_create(std::data(c_nodes()), std::size(c_nodes()), 42);
-            BOOST_SCOPE_EXIT(p_path)
-            {
-                tetengo_lattice_path_destroy(p_path);
-            }
-            BOOST_SCOPE_EXIT_END;
-
-            BOOST_TEST(p_path);
-        }
-        {
-            const auto* const p_path = tetengo_lattice_path_create(nullptr, 0, 42);
-            BOOST_SCOPE_EXIT(p_path)
-            {
-                tetengo_lattice_path_destroy(p_path);
-            }
-            BOOST_SCOPE_EXIT_END;
-
-            BOOST_TEST(p_path);
-        }
-        {
-            const auto* const p_path = tetengo_lattice_path_create(nullptr, 3, 42);
-
-            BOOST_TEST(!p_path);
         }
     }
     */
@@ -247,40 +188,6 @@ mod tests {
 
             BOOST_TEST(!std::empty(path_));
         }
-
-        {
-            const auto* const p_path = tetengo_lattice_path_createEmpty();
-            BOOST_SCOPE_EXIT(p_path)
-            {
-                tetengo_lattice_path_destroy(p_path);
-            }
-            BOOST_SCOPE_EXIT_END;
-
-            BOOST_TEST(tetengo_lattice_path_empty(p_path));
-        }
-        {
-            const auto* const p_path = tetengo_lattice_path_create(std::data(c_nodes()), std::size(c_nodes()), 42);
-            BOOST_SCOPE_EXIT(p_path)
-            {
-                tetengo_lattice_path_destroy(p_path);
-            }
-            BOOST_SCOPE_EXIT_END;
-
-            BOOST_TEST(!tetengo_lattice_path_empty(p_path));
-        }
-        {
-            const auto* const p_path = tetengo_lattice_path_create(nullptr, 0, 42);
-            BOOST_SCOPE_EXIT(p_path)
-            {
-                tetengo_lattice_path_destroy(p_path);
-            }
-            BOOST_SCOPE_EXIT_END;
-
-            BOOST_TEST(tetengo_lattice_path_empty(p_path));
-        }
-        {
-            BOOST_TEST(!tetengo_lattice_path_empty(nullptr));
-        }
     }
     */
     /*
@@ -298,48 +205,6 @@ mod tests {
 
             BOOST_CHECK(path_.nodes() == cpp_nodes());
         }
-
-        {
-            const auto* const p_path = tetengo_lattice_path_createEmpty();
-            BOOST_SCOPE_EXIT(p_path)
-            {
-                tetengo_lattice_path_destroy(p_path);
-            }
-            BOOST_SCOPE_EXIT_END;
-
-            const auto node_count = tetengo_lattice_path_pNodes(p_path, nullptr);
-            BOOST_TEST(node_count == 0U);
-        }
-        {
-            const auto* const p_path = tetengo_lattice_path_create(std::data(c_nodes()), std::size(c_nodes()), 42);
-            BOOST_SCOPE_EXIT(p_path)
-            {
-                tetengo_lattice_path_destroy(p_path);
-            }
-            BOOST_SCOPE_EXIT_END;
-
-            const auto node_count = tetengo_lattice_path_pNodes(p_path, nullptr);
-            BOOST_TEST_REQUIRE(node_count == std::size(c_nodes()));
-            std::vector<tetengo_lattice_node_t> nodes{ node_count };
-            const auto                          node_count_again = tetengo_lattice_path_pNodes(p_path, std::data(nodes));
-            BOOST_TEST(node_count_again == std::size(c_nodes()));
-            BOOST_TEST(equal_nodes(nodes, c_nodes()));
-        }
-        {
-            const auto* const p_path = tetengo_lattice_path_create(nullptr, 0, 42);
-            BOOST_SCOPE_EXIT(p_path)
-            {
-                tetengo_lattice_path_destroy(p_path);
-            }
-            BOOST_SCOPE_EXIT_END;
-
-            const auto node_count = tetengo_lattice_path_pNodes(p_path, nullptr);
-            BOOST_TEST(node_count == 0U);
-        }
-        {
-            const auto node_count = tetengo_lattice_path_pNodes(nullptr, nullptr);
-            BOOST_TEST(node_count == 0U);
-        }
     }
     */
     /*
@@ -356,40 +221,6 @@ mod tests {
             const tetengo::lattice::path path_{ cpp_nodes(), 42 };
 
             BOOST_TEST(path_.cost() == 42);
-        }
-
-        {
-            const auto* const p_path = tetengo_lattice_path_createEmpty();
-            BOOST_SCOPE_EXIT(p_path)
-            {
-                tetengo_lattice_path_destroy(p_path);
-            }
-            BOOST_SCOPE_EXIT_END;
-
-            BOOST_TEST(tetengo_lattice_path_cost(p_path) == 0);
-        }
-        {
-            const auto* const p_path = tetengo_lattice_path_create(std::data(c_nodes()), std::size(c_nodes()), 42);
-            BOOST_SCOPE_EXIT(p_path)
-            {
-                tetengo_lattice_path_destroy(p_path);
-            }
-            BOOST_SCOPE_EXIT_END;
-
-            BOOST_TEST(tetengo_lattice_path_cost(p_path) == 42);
-        }
-        {
-            const auto* const p_path = tetengo_lattice_path_create(nullptr, 0, 42);
-            BOOST_SCOPE_EXIT(p_path)
-            {
-                tetengo_lattice_path_destroy(p_path);
-            }
-            BOOST_SCOPE_EXIT_END;
-
-            BOOST_TEST(tetengo_lattice_path_cost(p_path) == 42);
-        }
-        {
-            BOOST_TEST(tetengo_lattice_path_cost(nullptr) == 0);
         }
     }
          */
