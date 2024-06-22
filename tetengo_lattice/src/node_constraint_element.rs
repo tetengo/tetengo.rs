19578262e0c83ce8adcdcd7cdffde3e460f8ff7e
kaoru <kaorut@mc.pikara.ne.jp>
2024-06-22 21:47:12 +0900

    Create NodeConstraintElement #65

---
+++ tetengo_lattice/src/node_constraint_element.rs
@@ -0,0 +1,14 @@
+/*!
+ * A node constraint element.
+ *
+ * Copyright (C) 2023-2024 kaoru  <https://www.tetengo.org/>
+ */
+
+/**
+ * A node constraint element.
+ */
+#[derive(Copy, Clone, Debug)]
+pub struct NodeConstraintElement {}
+
+#[cfg(test)]
+mod tests {}
