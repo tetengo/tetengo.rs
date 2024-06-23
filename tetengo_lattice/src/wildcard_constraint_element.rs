cf5e3f8c5739399794dbfa62111b73b291dc7134
kaoru <kaorut@mc.pikara.ne.jp>
2024-06-23 14:07:18 +0900

    Create WildcardConstraintElement #67

---
+++ tetengo_lattice/src/wildcard_constraint_element.rs
@@ -0,0 +1,16 @@
+/*!
+ * A wildcard constraint element.
+ *
+ * Copyright (C) 2023-2024 kaoru  <https://www.tetengo.org/>
+ */
+
+/**
+ * A wildcard constraint element.
+ */
+#[derive(Copy, Clone, Debug)]
+pub struct WildcardConstraintElement {}
+
+impl WildcardConstraintElement {}
+
+#[cfg(test)]
+mod tests {}
