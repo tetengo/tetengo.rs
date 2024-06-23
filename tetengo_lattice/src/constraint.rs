2defa47727d609749680450a5de4ea1d60cea4fc
kaoru <kaorut@mc.pikara.ne.jp>
2024-06-23 14:50:07 +0900

    Create Constraint #62

---
+++ tetengo_lattice/src/constraint.rs
@@ -0,0 +1,14 @@
+/*!
+ * A constraint.
+ *
+ * Copyright (C) 2023-2024 kaoru  <https://www.tetengo.org/>
+ */
+
+/**
+ * A constraint.
+ */
+#[derive(Clone, Copy, Debug)]
+pub struct Constraint {}
+
+#[cfg(test)]
+mod tests {}
