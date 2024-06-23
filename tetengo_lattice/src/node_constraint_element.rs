9c95939b42d5346c6eb3d9e63f3c3dc5ac756033
kaoru <kaorut@mc.pikara.ne.jp>
2024-06-23 12:37:49 +0900

    Implement NodeConstraintElement #65

--- tetengo_lattice/src/node_constraint_element.rs
+++ tetengo_lattice/src/node_constraint_element.rs
@@ -4,6 +4,7 @@
  * Copyright (C) 2023-2024 kaoru  <https://www.tetengo.org/>
  */
 
+use crate::constraint_element::ConstraintElement;
 use crate::node::Node;
 
 /**
@@ -11,7 +12,7 @@ use crate::node::Node;
  */
 #[derive(Clone, Debug)]
 pub struct NodeConstraintElement<'a> {
-    _node: Node<'a>,
+    node: Node<'a>,
 }
 
 impl<'a> NodeConstraintElement<'a> {
@@ -22,16 +23,20 @@ impl<'a> NodeConstraintElement<'a> {
      * * `node` - A node.
      */
     pub fn new(node: Node<'a>) -> Self {
-        Self { _node: node }
+        Self { node }
     }
-
-    /*
-            int matches_impl(const node& node_) const
-            {
-                return node_ == m_node ? 0 : -1;
-            }
-    */
 }
+
+impl ConstraintElement for NodeConstraintElement<'_> {
+    fn matches(&self, node: &Node<'_>) -> i32 {
+        if *node == self.node {
+            0
+        } else {
+            -1
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::rc::Rc;
@@ -58,38 +63,38 @@ mod tests {
         let _element = NodeConstraintElement::new(element_node);
     }
 
-    /*
-    BOOST_AUTO_TEST_CASE(matches)
-    {
-        BOOST_TEST_PASSPOINT();
+    #[test]
+    fn matches() {
+        let element_node_key = StringInput::new(String::from("mizuho"));
+        let element_node_value = 42;
+        let element_node_preceding_edge_costs = Rc::new(vec![3, 1, 4, 1, 5, 9, 2, 6]);
+        let element_node = Node::new(
+            &element_node_key,
+            &element_node_value,
+            0,
+            1,
+            element_node_preceding_edge_costs,
+            5,
+            24,
+            2424,
+        );
+        let element = NodeConstraintElement::new(element_node);
 
         {
-            const tetengo::lattice::string_input element_node_key{ "mizuho" };
-            const std::any                       element_node_value{ 42 };
-            const std::vector<int>               element_node_preceding_edge_costs{ 3, 1, 4, 1, 5, 9, 2, 6 };
-            tetengo::lattice::node               element_node{
-                &element_node_key, &element_node_value, 0, 1, &element_node_preceding_edge_costs, 5, 24, 2424
-            };
-            const tetengo::lattice::node_constraint_element element{ std::move(element_node) };
+            let key = StringInput::new(String::from("mizuho"));
+            let value = 42;
+            let preceding_edge_costs = Rc::new(vec![3, 1, 4, 1, 5, 9, 2, 6]);
+            let node = Node::new(&key, &value, 0, 1, preceding_edge_costs, 5, 24, 2424);
 
-            {
-                const tetengo::lattice::string_input key{ "mizuho" };
-                const std::any                       value{ 42 };
-                const std::vector<int>               preceding_edge_costs{ 3, 1, 4, 1, 5, 9, 2, 6 };
-                const tetengo::lattice::node         node_{ &key, &value, 0, 1, &preceding_edge_costs, 5, 24, 2424 };
-
-                BOOST_TEST(element.matches(node_) == 0);
-            }
-            {
-                const tetengo::lattice::string_input key{ "sakura" };
-                const std::any                       value{ 42 };
-                const std::vector<int>               preceding_edge_costs{ 3, 1, 4, 1, 5, 9, 2, 6 };
-                const tetengo::lattice::node         node_{ &key, &value, 0, 1, &preceding_edge_costs, 5, 24, 2424 };
-
-                BOOST_TEST(element.matches(node_) < 0);
-            }
+            assert_eq!(element.matches(&node), 0);
         }
+        {
+            let key = StringInput::new(String::from("sakura"));
+            let value = 42;
+            let preceding_edge_costs = Rc::new(vec![3, 1, 4, 1, 5, 9, 2, 6]);
+            let node = Node::new(&key, &value, 0, 1, preceding_edge_costs, 5, 24, 2424);
 
+            assert!(element.matches(&node) < 0);
+        }
     }
-     */
 }
