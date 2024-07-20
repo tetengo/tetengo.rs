3fc2abd7a90c0a291a47165a7e08c8dc45ed1097
kaoru <kaorut@mc.pikara.ne.jp>
2024-07-20 16:27:09 +0900

    Implement list_sources.py #83

--- scripts/sort_derive_attributes.py
+++ scripts/sort_derive_attributes.py
@@ -2,4 +2,18 @@
 # Sorts the traits listed for #derive attributes.
 # Copyright (C) 2023-2024 kaoru  <https://www.tetengo.org>
 
-print("Hello, world!")
+import sys
+from pathlib import Path
+
+import list_sources
+
+
+def main(args: list[str]):
+    package_root = Path(sys.argv[0]) / Path("..")
+    paths = list_sources.list_rs_files(package_root)
+    for path in paths:
+        print("Path: {}".format(path))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
